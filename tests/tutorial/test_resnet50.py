0afa8d90073179efda1ebe73b0dca9c9b2f229cb
parroteffect <parroteffect@gmail.com>
2025-04-07 18:46:12 +0900

    checked model functionality

--- tests/tutorial/test_resnet50.py
+++ tests/tutorial/test_resnet50.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     image_path = args.image_path
 
     resnet50 = ResNet50(
+        local_model="/workspace/mblt-model-zoo/tmp/resnet50.mxq",
         model_type="IMAGENET1K_V1",
     )
 
