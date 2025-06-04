c8e852e676c36d6f188a0030ed9ad967946bdcc3
jinman <jinman@mobilint.com>
2025-06-04 17:02:16 +0900

    update for whisper

---
+++ mblt_model_zoo/stt/whisper/prepare.py
@@ -0,0 +1,24 @@
+from huggingface_hub import snapshot_download
+import os
+from ...vision.utils.downloads import *
+
+def prepare_files(REPO_ID = "openai/whisper-small",
+                  base_dir = "https://dl.mobilint.com/model/stt/whisper-small",
+                  encoder_mxq = "whisper-small_encoder.mxq",
+                  decoder_mxq = "whisper-small_decoder.mxq",
+                  model_cfg = "config.json"):
+    
+    HOME_PATH = os.path.expanduser("~")
+    MODEL_NAME = REPO_ID.strip("/").split("/")[-1]
+    MODEL_PATH = f"{HOME_PATH}/.mblt_model_zoo/{MODEL_NAME}"
+    
+    snapshot_download(repo_id=REPO_ID,
+                  local_dir=MODEL_PATH)
+    
+    download_url_to_file(url=f"{base_dir}/{encoder_mxq}",
+                     dst=f"{MODEL_PATH}/{encoder_mxq}")
+    download_url_to_file(url=f"{base_dir}/{decoder_mxq}",
+                        dst=f"{MODEL_PATH}/{decoder_mxq}")
+    download_url_to_file(url=f"{base_dir}/config.json",
+                        dst=f"{MODEL_PATH}/{model_cfg}")
+    
