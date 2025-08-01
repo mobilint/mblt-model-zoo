642a1f76db59e0ce7b7ec916ce517dc1112ef5d1
jinman <jinman@mobilint.com>
2025-08-01 13:39:04 +0900

    hotfix

--- mblt_model_zoo/vision/image_classification/densenet.py
+++ mblt_model_zoo/vision/image_classification/densenet.py
@@ -33,37 +33,6 @@ class DenseNet121_Set(ModelInfoSet):
     DEFAULT = IMAGENET1K_V1  # Default model
 
 
-class DenseNet161_Set(ModelInfoSet):
-    IMAGENET1K_V1 = ModelInfo(
-        model_cfg={
-            "url_dict": {
-                "aries": {
-                    "single": "https://dl.mobilint.com/model/vision/image_classification/densenet161_IMAGENET1K_V1/aries/single/densenet161_IMAGENET1K_V1.mxq",
-                    "multi": "https://dl.mobilint.com/model/vision/image_classification/densenet161_IMAGENET1K_V1/aries/multi/densenet161_IMAGENET1K_V1.mxq",
-                    "global": "https://dl.mobilint.com/model/vision/image_classification/densenet161_IMAGENET1K_V1/aries/global/densenet161_IMAGENET1K_V1.mxq",
-                },
-                "regulus": {"single": None},
-            },
-        },
-        pre_cfg={
-            "Reader": {
-                "style": "pil",
-            },
-            "Resize": {
-                "size": 256,
-                "interpolation": "bilinear",
-            },
-            "CenterCrop": {
-                "size": [224, 224],
-            },
-            "Normalize": {"style": "torch"},
-            "SetOrder": {"shape": "CHW"},
-        },
-        post_cfg={"task": "image_classification"},
-    )
-    DEFAULT = IMAGENET1K_V1  # Default model
-
-
 class DenseNet169_Set(ModelInfoSet):
     IMAGENET1K_V1 = ModelInfo(
         model_cfg={
@@ -134,21 +103,6 @@ def DenseNet121(
 ) -> MBLT_Engine:
     return MBLT_Engine.from_model_info_set(
         DenseNet121_Set,
-        local_path=local_path,
-        model_type=model_type,
-        infer_mode=infer_mode,
-        product=product,
-    )
-
-
-def DenseNet161(
-    local_path: str = None,
-    model_type: str = "DEFAULT",
-    infer_mode: str = "global",
-    product: str = "aries",
-) -> MBLT_Engine:
-    return MBLT_Engine.from_model_info_set(
-        DenseNet161_Set,
         local_path=local_path,
         model_type=model_type,
         infer_mode=infer_mode,
--- mblt_model_zoo/vision/instance_segmentation/yolov5_seg.py
+++ mblt_model_zoo/vision/instance_segmentation/yolov5_seg.py
@@ -2,41 +2,6 @@ from ..utils.types import ModelInfo, ModelInfoSet
 from ..wrapper import MBLT_Engine
 
 
-class YOLOv5mSeg_Set(ModelInfoSet):
-    COCO_V1 = ModelInfo(
-        model_cfg={
-            "url_dict": {
-                "aries": {
-                    "single": "https://dl.mobilint.com/model/vision/instance_segmentation/yolov5m-seg/aries/single/yolov5m-seg.mxq",
-                    "multi": "https://dl.mobilint.com/model/vision/instance_segmentation/yolov5m-seg/aries/multi/yolov5m-seg.mxq",
-                    "global": "https://dl.mobilint.com/model/vision/instance_segmentation/yolov5m-seg/aries/global/yolov5m-seg.mxq",
-                },
-                "regulus": {"single": None},
-            },
-        },
-        pre_cfg={
-            "Reader": {
-                "style": "numpy",
-            },
-            "YoloPre": {
-                "img_size": [640, 640],
-            },
-            "SetOrder": {"shape": "CHW"},
-        },
-        post_cfg={
-            "task": "instance_segmentation",
-            "nc": 80,  # Number of classes
-            "anchors": [
-                [10, 13, 16, 30, 33, 23],  # P3/8
-                [30, 61, 62, 45, 59, 119],  # P4/16
-                [116, 90, 156, 198, 373, 326],  # P5/32
-            ],
-            "n_extra": 32,
-        },
-    )
-    DEFAULT = COCO_V1
-
-
 class YOLOv5lSeg_Set(ModelInfoSet):
     COCO_V1 = ModelInfo(
         model_cfg={
@@ -105,21 +70,6 @@ class YOLOv5xSeg_Set(ModelInfoSet):
         },
     )
     DEFAULT = COCO_V1
-
-
-def YOLOv5mSeg(
-    local_path: str = None,
-    model_type: str = "DEFAULT",
-    infer_mode: str = "global",
-    product: str = "aries",
-) -> MBLT_Engine:
-    return MBLT_Engine.from_model_info_set(
-        YOLOv5mSeg_Set,
-        local_path=local_path,
-        model_type=model_type,
-        infer_mode=infer_mode,
-        product=product,
-    )
 
 
 def YOLOv5lSeg(
