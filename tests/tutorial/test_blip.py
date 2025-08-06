e2a159fb07c74cedca35cf046efc793efb24cdef
beomsu <beomsu@mobilint.com>
2025-08-06 13:49:51 +0900

    Fix test_blip.py

--- tests/tutorial/test_blip.py
+++ tests/tutorial/test_blip.py
@@ -1,15 +1,15 @@
 import requests
 from PIL import Image
-from mblt_model_zoo.transformers import pipeline, AutoTokenizer
+from mblt_model_zoo.transformers import pipeline, AutoProcessor
 from transformers import TextStreamer
 
 model_name = "mobilint/blip-image-captioning-large"
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+processor = AutoProcessor.from_pretrained(model_name, use_fast=True)
 pipe = pipeline(
     "image-text-to-text",
     model=model_name,
-    streamer=TextStreamer(tokenizer=tokenizer, skip_prompt=False),
+    processor=processor,
 )
 
 img_url = "https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg"
@@ -17,7 +17,19 @@ raw_image = Image.open(requests.get(img_url, stream=True).raw).convert("RGB")
 
 # conditional image captioning
 text = "a photography of"
-pipe(raw_image, text)
+pipe(
+    raw_image, text,
+    generate_kwargs={
+        "max_length": 4096,
+        "streamer": TextStreamer(tokenizer=pipe.tokenizer, skip_prompt=False),
+    },
+)
 
 # unconditional image captioning
-pipe(raw_image, "")
+pipe(
+    raw_image, "",
+    generate_kwargs={
+        "max_length": 4096,
+        "streamer": TextStreamer(tokenizer=pipe.tokenizer, skip_prompt=False),
+    },
+)
