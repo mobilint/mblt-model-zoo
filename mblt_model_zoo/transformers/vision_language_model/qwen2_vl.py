63cc3fc08829c69e92da877f2dee32b09d77fe7b
beomsu <beomsu@mobilint.com>
2025-08-12 14:06:08 +0900

    Fix chunk_size param

--- mblt_model_zoo/transformers/vision_language_model/qwen2_vl.py
+++ mblt_model_zoo/transformers/vision_language_model/qwen2_vl.py
@@ -231,6 +231,7 @@ class MobilintQwen2VLTextModel(MobilintQwen2VLPreTrainedModel):
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
         cache_position: Optional[torch.LongTensor] = None,
+        chunk_size: int = 0,
     ) -> Union[tuple, BaseModelOutputWithPast]:
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
