39a200de93fe08502b5e8fb02b99f76b24b30950
beomsu <beomsu@mobilint.com>
2025-08-25 18:25:33 +0900

    Fix prepare cache

--- mblt_model_zoo/transformers/utils/generation_utils.py
+++ mblt_model_zoo/transformers/utils/generation_utils.py
@@ -1,7 +1,8 @@
+from typing import Dict
 import torch
 import maccel
 from abc import ABC, abstractmethod
-from transformers import Cache, GenerationMixin
+from transformers import Cache, GenerationConfig, GenerationMixin, PreTrainedModel
 from mblt_model_zoo.transformers.utils.cache_utils import MobilintCache
 
 class MobilintGenerationMixin(ABC, GenerationMixin):        
@@ -18,3 +19,34 @@ class MobilintGenerationMixin(ABC, GenerationMixin):
             self._cache.reset()
             
         return self._cache
+
+    def _prepare_cache_for_generation(
+        self,
+        generation_config: GenerationConfig,
+        model_kwargs: Dict,
+        assistant_model: "PreTrainedModel",
+        batch_size: int,
+        max_cache_length: int,
+        device: torch.device,
+    ) -> bool:
+        super()._prepare_cache_for_generation(
+            generation_config,
+            model_kwargs,
+            assistant_model,
+            batch_size,
+            max_cache_length,
+            device,
+        )
+
+        cache_name = "past_key_values"
+
+        if model_kwargs.get(cache_name, None) is None:
+            return
+        elif model_kwargs[cache_name].__class__.__name__ == "MobilintCache":
+            return
+        elif model_kwargs[cache_name].__class__.__name__ == "DynamicCache":
+            model_kwargs[cache_name] = self._get_cache("mobilint", batch_size, max_cache_length, device)
+        else:
+            raise NotImplementedError(
+                f"_prepare_cache_for_generation Cache class {model_kwargs[cache_name].__class__.__name__}, which is not compatible for MobilintCache"
+            )
