0f8bd28458676a2d9067098d22a0f194d72a2192
beomsu <beomsu@mobilint.com>
2025-09-26 11:36:51 +0900

    Add test_melo.py

---
+++ tests/transformers/test_melo.py
@@ -0,0 +1,31 @@
+from mblt_model_zoo.transformers.text_to_speech.melo import TTS
+
+# Speed is adjustable
+speed = 1.0
+
+device = 'auto'
+
+# English 
+text = "Did you ever hear a folk tale about a giant turtle?"
+model = TTS(language='EN', device=device)
+speaker_ids = model.hps.data.spk2id
+
+# American accent
+output_path = 'en-us.wav'
+model.tts_to_file(text, speaker_ids['EN-US'], output_path, speed=speed)
+
+# British accent
+output_path = 'en-br.wav'
+model.tts_to_file(text, speaker_ids['EN-BR'], output_path, speed=speed)
+
+# Indian accent
+output_path = 'en-india.wav'
+model.tts_to_file(text, speaker_ids['EN_INDIA'], output_path, speed=speed)
+
+# Australian accent
+output_path = 'en-au.wav'
+model.tts_to_file(text, speaker_ids['EN-AU'], output_path, speed=speed)
+
+# Default accent
+output_path = 'en-default.wav'
+model.tts_to_file(text, speaker_ids['EN-Default'], output_path, speed=speed)
