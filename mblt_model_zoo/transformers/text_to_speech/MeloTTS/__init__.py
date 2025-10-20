667057be761d912d99ff66ba913d0d127e6eea92
beomsu <beomsu@mobilint.com>
2025-10-20 18:52:55 +0900

    Fix import error when there is no MeloTTS package

--- mblt_model_zoo/transformers/text_to_speech/MeloTTS/__init__.py
+++ mblt_model_zoo/transformers/text_to_speech/MeloTTS/__init__.py
@@ -1,2 +1,14 @@
 from .api import TTS
-from .download_utils import EN_NEWEST
+from ...utils.types import TransformersModelInfo
+
+EN_NEWEST = TransformersModelInfo(
+    original_model_id="myshell-ai/MeloTTS-English-v3",
+    model_id="mobilint/MeloTTS-English-v3",
+    download_url_base="https://dl.mobilint.com/model/transformers/tts/MeloTTS-English-v3/",
+    file_list=[
+        "checkpoint.pth",
+        "config.json",
+        "MeloTTS-English-v3_enc_p_sdp_dp.mxq",
+        "MeloTTS-English-v3_dec_flow.mxq",
+    ],
+)
