2139ad00133447a7cdb35a2729ece4b5d09975df
beomsu <beomsu@mobilint.com>
2025-10-21 13:15:46 +0900

    Finish MobilintBertModel

--- mblt_model_zoo/transformers/large_language_model/bert.py
+++ mblt_model_zoo/transformers/large_language_model/bert.py
@@ -1,12 +1,10 @@
-from typing import Optional, Tuple, Union
+from typing import Optional, Union
 
 import maccel
 import torch
-import torch.nn as nn
-import numpy as np
-import math
 
 from transformers import (
+    Cache,
     BertPreTrainedModel,
     BertConfig,
     BertTokenizerFast,
@@ -15,11 +13,10 @@ from transformers import (
     AutoTokenizer,
     AutoModelForMaskedLM,
 )
-from transformers.processing_utils import Unpack
-from transformers.modeling_outputs import MaskedLMOutputWithPast
-from transformers.utils import TransformersKwargs, logging
+from transformers.models.bert.modeling_bert import BertEmbeddings, BertPooler
+from transformers.modeling_outputs import BaseModelOutputWithPoolingAndCrossAttentions
+from transformers.utils import logging
 
-from mblt_model_zoo.transformers.utils.generation_utils import MobilintGenerationMixin
 from ..utils.cache_utils import MobilintCache
 
 
@@ -54,20 +51,24 @@ class MobilintBertPreTrainedModel(BertPreTrainedModel):
 class MobilintBertModel(MobilintBertPreTrainedModel):
     _no_split_modules = ["BertEmbeddings", "BertLayer"]
 
-    def __init__(self, config, add_pooling_layer=True):
+    def __init__(self, config: MobilintBertConfig, add_pooling_layer=True):
         super().__init__(config)
         self.config = config
 
         self.embeddings = BertEmbeddings(config)
-        self.encoder = BertEncoder(config)
 
         self.pooler = BertPooler(config) if add_pooling_layer else None
 
         self.attn_implementation = config._attn_implementation
         self.position_embedding_type = config.position_embedding_type
 
-        # Initialize weights and apply final processing
-        self.post_init()
+        self.dev_no = config.dev_no
+        self.acc = maccel.Accelerator(self.dev_no)
+        mc = maccel.ModelConfig()
+        mc.set_single_core_mode(1)
+        self.mxq_model = maccel.Model(f"{config.name_or_path}/{config.mxq_path}", mc)
+        self.mxq_model.launch(self.acc)
+        self.mxq_model.reset_cache_memory()
 
     def get_input_embeddings(self):
         return self.embeddings.word_embeddings
@@ -89,7 +90,7 @@ class MobilintBertModel(MobilintBertPreTrainedModel):
         inputs_embeds: Optional[torch.Tensor] = None,
         encoder_hidden_states: Optional[torch.Tensor] = None,
         encoder_attention_mask: Optional[torch.Tensor] = None,
-        past_key_values: Optional[list[torch.FloatTensor]] = None,
+        past_key_values: Optional[MobilintCache] = None,
         use_cache: Optional[bool] = None,
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
@@ -101,6 +102,24 @@ class MobilintBertModel(MobilintBertPreTrainedModel):
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if attention_mask is not None:
+            logger.warning_once("attention_mask is not supported.")
+
+        if head_mask is not None:
+            logger.warning_once("head_mask is not supported.")
+
+        if encoder_hidden_states is not None:
+            logger.warning_once("encoder_hidden_states is not supported.")
+            
+        if encoder_attention_mask is not None:
+            logger.warning_once("encoder_attention_mask is not supported.")
+
+        if output_attentions:
+            logger.warning_once("output_attentions is not supported.")
+
+        if output_hidden_states:
+            logger.warning_once("output_hidden_states is not supported.")
 
         if self.config.is_decoder:
             use_cache = use_cache if use_cache is not None else self.config.use_cache
@@ -143,90 +162,40 @@ class MobilintBertModel(MobilintBertPreTrainedModel):
             inputs_embeds=inputs_embeds,
             past_key_values_length=past_key_values_length,
         )
+        
+        if use_cache and past_key_values is None:
+            past_key_values = MobilintCache(self.mxq_model)
+        
+        past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
+        
+        if cache_position is None:
+            cache_position = torch.arange(
+                past_seen_tokens,
+                past_seen_tokens + embedding_output.shape[1],
+                device=embedding_output.device,
+            )
 
-        if attention_mask is None:
-            attention_mask = torch.ones((batch_size, seq_length + past_key_values_length), device=device)
+        sequence_output = self.mxq_model.infer([embedding_output], None, past_seen_tokens if use_cache else 0)[0]
+        if use_cache:
+            past_key_values.update_cache_position(cache_position)
 
-        use_sdpa_attention_masks = (
-            self.attn_implementation == "sdpa"
-            and self.position_embedding_type == "absolute"
-            and head_mask is None
-            and not output_attentions
-        )
-
-        # Expand the attention mask
-        if use_sdpa_attention_masks and attention_mask.dim() == 2:
-            # Expand the attention mask for SDPA.
-            # [bsz, seq_len] -> [bsz, 1, seq_len, seq_len]
-            if self.config.is_decoder:
-                extended_attention_mask = _prepare_4d_causal_attention_mask_for_sdpa(
-                    attention_mask,
-                    input_shape,
-                    embedding_output,
-                    past_key_values_length,
-                )
-            else:
-                extended_attention_mask = _prepare_4d_attention_mask_for_sdpa(
-                    attention_mask, embedding_output.dtype, tgt_len=seq_length
-                )
-        else:
-            # We can provide a self-attention mask of dimensions [batch_size, from_seq_length, to_seq_length]
-            # ourselves in which case we just need to make it broadcastable to all heads.
-            extended_attention_mask = self.get_extended_attention_mask(attention_mask, input_shape)
-
-        # If a 2D or 3D attention mask is provided for the cross-attention
-        # we need to make broadcastable to [batch_size, num_heads, seq_length, seq_length]
-        if self.config.is_decoder and encoder_hidden_states is not None:
-            encoder_batch_size, encoder_sequence_length, _ = encoder_hidden_states.size()
-            encoder_hidden_shape = (encoder_batch_size, encoder_sequence_length)
-            if encoder_attention_mask is None:
-                encoder_attention_mask = torch.ones(encoder_hidden_shape, device=device)
-
-            if use_sdpa_attention_masks and encoder_attention_mask.dim() == 2:
-                # Expand the attention mask for SDPA.
-                # [bsz, seq_len] -> [bsz, 1, seq_len, seq_len]
-                encoder_extended_attention_mask = _prepare_4d_attention_mask_for_sdpa(
-                    encoder_attention_mask, embedding_output.dtype, tgt_len=seq_length
-                )
-            else:
-                encoder_extended_attention_mask = self.invert_attention_mask(encoder_attention_mask)
-        else:
-            encoder_extended_attention_mask = None
-
-        # Prepare head mask if needed
-        # 1.0 in head_mask indicate we keep the head
-        # attention_probs has shape bsz x n_heads x N x N
-        # input head_mask has shape [num_heads] or [num_hidden_layers x num_heads]
-        # and head_mask is converted to shape [num_hidden_layers x batch x num_heads x seq_length x seq_length]
-        head_mask = self.get_head_mask(head_mask, self.config.num_hidden_layers)
-
-        encoder_outputs = self.encoder(
-            embedding_output,
-            attention_mask=extended_attention_mask,
-            head_mask=head_mask,
-            encoder_hidden_states=encoder_hidden_states,
-            encoder_attention_mask=encoder_extended_attention_mask,
-            past_key_values=past_key_values,
-            use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-            cache_position=cache_position,
-        )
-        sequence_output = encoder_outputs[0]
+        sequence_output = torch.tensor(sequence_output, dtype=torch.float32).squeeze(0)
         pooled_output = self.pooler(sequence_output) if self.pooler is not None else None
 
         if not return_dict:
-            return (sequence_output, pooled_output) + encoder_outputs[1:]
+            return (sequence_output, pooled_output)
 
         return BaseModelOutputWithPoolingAndCrossAttentions(
             last_hidden_state=sequence_output,
             pooler_output=pooled_output,
-            past_key_values=encoder_outputs.past_key_values,
-            hidden_states=encoder_outputs.hidden_states,
-            attentions=encoder_outputs.attentions,
-            cross_attentions=encoder_outputs.cross_attentions,
+            past_key_values=past_key_values,
+            hidden_states=None,
+            attentions=None,
+            cross_attentions=None,
         )
+
+    def dispose(self):
+        self.mxq_model.dispose()
 
 
 class MobilintBertForMaskedLM(MobilintBertPreTrainedModel):
