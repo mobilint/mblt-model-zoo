674dcdc3545649b2f9472d79becf866d189f974e
beomsu <beomsu@mobilint.com>
2025-10-21 14:46:26 +0900

    Update test_bert.py

--- tests/tutorial/test_bert.py
+++ tests/tutorial/test_bert.py
@@ -1,8 +1,9 @@
 from mblt_model_zoo.transformers import pipeline
+from pprint import pprint
 
 model_name = "mobilint/bert-base-uncased"
 
 pipe = pipeline('fill-mask', model=model_name)
 
 output = pipe("Hello I'm a [MASK] model.")
-print(output)
+pprint(output)
