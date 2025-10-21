0b93f54f9394f5e64b108df0d08a48954d43d69d
beomsu <beomsu@mobilint.com>
2025-10-21 13:41:00 +0900

    Add test_bert.py

---
+++ tests/tutorial/test_bert.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+
+model_name = "mobilint/bert-base-uncased"
+
+pipe = pipeline('fill-mask', model=model_name)
+
+output = pipe("Hello I'm a [MASK] model.")
+print(output)
