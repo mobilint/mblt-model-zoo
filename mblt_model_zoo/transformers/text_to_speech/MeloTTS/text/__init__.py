715ca94119526dd72ad00f4fac8c7b7fdaa41709
beomsu <beomsu@mobilint.com>
2025-10-21 19:36:13 +0900

    Add more debugging

--- mblt_model_zoo/transformers/text_to_speech/MeloTTS/text/__init__.py
+++ mblt_model_zoo/transformers/text_to_speech/MeloTTS/text/__init__.py
@@ -33,6 +33,7 @@ def get_bert_feature(text, word2ph, device=None, model_id=''):
             inputs[i] = inputs[i].to(device)
         # bert models are compiled to output third-from-last hidden state
         res = model(**inputs)
+        print("get_bert_feature", res[0].shape)
         res = torch.cat((res[0],), -1)[0].cpu()
         
     assert inputs["input_ids"].shape[-1] == len(word2ph)
