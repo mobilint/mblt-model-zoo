630aae59f4a1c96cdddbf0186aedf7fde5c7cb6d
beomsu <beomsu@mobilint.com>
2025-11-20 13:28:28 +0900

    Roll back test exaone deep

--- tests/transformers/AutoModelForCausalLM/test_exaone_deep.py
+++ tests/transformers/AutoModelForCausalLM/test_exaone_deep.py
@@ -39,7 +39,7 @@ def test_exaone_deep(pipe):
 \[\log_2\left({x \over yz}\right) = {1 \over 2}\]\[\log_2\left({y \over xz}\right) = {1 \over 3}\]\[\log_2\left({z \over xy}\right) = {1 \over 4}\]
 Then the value of $\left|\log_2(x^4y^3z^2)\right|$ is $\tfrac{m}{n}$ where $m$ and $n$ are relatively prime positive integers. Find $m+n$.
 
-Please reason as short as possible, and put your final answer within \boxed{}."""
+Please reason step by step, and put your final answer within \boxed{}."""
     #   Korean MCQA example (CSAT Math 2025)
     prompt = r"""Question : $a_1 = 2$인 수열 $\{a_n\}$과 $b_1 = 2$인 등차수열 $\{b_n\}$이 모든 자연수 $n$에 대하여\[\sum_{k=1}^{n} \frac{a_k}{b_{k+1}} = \frac{1}{2} n^2\]을 만족시킬 때, $\sum_{k=1}^{5} a_k$의 값을 구하여라.
 
@@ -50,11 +50,11 @@ C) 130
 D) 135
 E) 140
  
-Please reason as short as possible, and you should write the correct option alphabet (A, B, C, D or E) within \\boxed{}."""
+Please reason step by step, and you should write the correct option alphabet (A, B, C, D or E) within \\boxed{}."""
 
     messages = [{"role": "user", "content": prompt}]
 
     outputs = pipe(
         messages,
-        max_length=4096,
+        max_length=512,
     )
