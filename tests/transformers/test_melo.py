8bb30f5942129a0674b88b6682f715e729a5edb3
beomsu <beomsu@mobilint.com>
2025-11-24 15:42:26 +0900

    Update test_melo.py for pytest

--- tests/transformers/test_melo.py
+++ tests/transformers/test_melo.py
@@ -1,15 +1,24 @@
+import pytest
+
 from mblt_model_zoo.transformers.text_to_speech.MeloTTS import TTS
 
-# Speed is adjustable
-speed = 1.0
 
-device = 'auto'
+@pytest.fixture
+def pipe():
+    pipe = TTS(language="EN_NEWEST", device="auto")
+    yield pipe
+    pipe.dispose()
 
-# English 
-text = "Did you ever hear a folk tale about a giant turtle?"
-model = TTS(language='EN_NEWEST', device=device)
-speaker_ids = model.hps.data.spk2id
 
-# American accent
-output_path = 'en-us.wav'
-model.tts_to_file(text, speaker_ids['EN-Newest'], output_path, speed=speed)
+def test_melo(pipe):
+    # Speed is adjustable
+    speed = 1.0
+
+    # English
+    text = "Did you ever hear a folk tale about a giant turtle?"
+
+    speaker_ids = pipe.hps.data.spk2id
+
+    # American accent
+    output_path = "en-us.wav"
+    pipe.tts_to_file(text, speaker_ids["EN-Newest"], output_path, speed=speed)
