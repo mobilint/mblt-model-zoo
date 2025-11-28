40536655db2a5ed9fa623483fe33af5a6028cd7c
beomsu <beomsu@mobilint.com>
2025-11-28 14:44:54 +0900

    Move original MeloTTS codes

--- mblt_model_zoo/transformers/text_to_speech/MeloTTS/text/__init__.py
+++ mblt_model_zoo/transformers/text_to_speech/MeloTTS/text/__init__.py
@@ -1,10 +1,31 @@
-import torch
 import sys
+
+import torch
+
+from .symbols import *
+
+_symbol_to_id = {s: i for i, s in enumerate(symbols)}
+
+
+def cleaned_text_to_sequence(cleaned_text, tones, language, symbol_to_id=None):
+    """Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
+    Args:
+      text: string to convert to a sequence
+    Returns:
+      List of integers corresponding to the symbols in the text
+    """
+    symbol_to_id_map = symbol_to_id if symbol_to_id else _symbol_to_id
+    phones = [symbol_to_id_map[symbol] for symbol in cleaned_text]
+    tone_start = language_tone_start_map[language]
+    tones = [i + tone_start for i in tones]
+    lang_id = language_id_map[language]
+    lang_ids = [lang_id for i in phones]
+    return phones, tones, lang_ids
 
 models = {}
 tokenizers = {}
 def get_bert_feature(text, word2ph, device=None, model_id=''):
-    from mblt_model_zoo.transformers import AutoTokenizer, AutoModelForMaskedLM
+    from mblt_model_zoo.transformers import AutoModelForMaskedLM, AutoTokenizer
     global model
     global tokenizer
     
