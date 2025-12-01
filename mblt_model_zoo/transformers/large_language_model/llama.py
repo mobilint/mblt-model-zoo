7de162c6a87944366f46c58d8f575427def2b96c
beomsu <beomsu@mobilint.com>
2025-12-01 15:03:55 +0900

    Refactor llama

--- mblt_model_zoo/transformers/large_language_model/llama.py
+++ mblt_model_zoo/transformers/large_language_model/llama.py
@@ -1,6 +1,6 @@
 import math
 import os
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Union, cast
 
 import maccel
 import numpy as np
@@ -17,7 +17,8 @@ from transformers import (
 )
 from transformers.modeling_outputs import CausalLMOutputWithPast
 from transformers.processing_utils import Unpack
-from transformers.utils import TransformersKwargs, logging
+from transformers.utils import logging
+from transformers.utils.generic import TransformersKwargs
 
 from mblt_model_zoo.transformers.utils.generation_utils import MobilintGenerationMixin
 from mblt_model_zoo.utils.logging import log_model_details
@@ -69,6 +70,7 @@ class MobilintLlamaForCausalLM(LlamaPreTrainedModel, MobilintGenerationMixin):
         self.acc = maccel.Accelerator(self.dev_no)
         mc = maccel.ModelConfig()
         mc.set_single_core_mode(1)
+        assert config.name_or_path is not None
         model_path = os.path.join(config.name_or_path, config.mxq_path)
         self.mxq_model = maccel.Model(model_path, mc)
         log_model_details(model_path)
@@ -78,10 +80,10 @@ class MobilintLlamaForCausalLM(LlamaPreTrainedModel, MobilintGenerationMixin):
         return self.mxq_model
 
     def set_decoder(self, decoder):
-        raise NotImplementedError("self.model is implemented in mxq")
+        raise NotImplementedError("set_decoder not available: self.model is implemented in mxq")
 
     def get_decoder(self):
-        logger.warning_once("self.model is implemented in mxq")
+        logger.warning("get_decoder not available: self.model is implemented in mxq")
         return None
 
     def tie_weights(self):
@@ -102,77 +104,51 @@ class MobilintLlamaForCausalLM(LlamaPreTrainedModel, MobilintGenerationMixin):
         **kwargs: Unpack[TransformersKwargs],
     ) -> Union[Tuple, CausalLMOutputWithPast]:
         if logits_to_keep > 1:
-            logger.warning(
-                "logits_to_keep larger than 1 is not supported: %d"
-                % logits_to_keep
-            )
+            logger.warning("logits_to_keep larger than 1 is not supported: %d" % logits_to_keep)
 
         if (input_ids is None) ^ (inputs_embeds is not None):
-            raise ValueError(
-                "You must specify exactly one of input_ids or inputs_embeds"
-            )
+            raise ValueError("You must specify exactly one of input_ids or inputs_embeds")
 
         if inputs_embeds is None:
-            inputs_embeds = self.embed_tokens(input_ids)
-
+            inputs_embeds: torch.FloatTensor = self.embed_tokens(input_ids)
+        
         if use_cache and past_key_values is None:
-            past_key_values = MobilintCache(self.mxq_model)
+            past_key_values = MobilintCache(self.get_cache_mxq_model())
 
         if cache_position is None:
-            past_seen_tokens = (
-                past_key_values.get_seq_length() if past_key_values is not None else 0
-            )
-            cache_position = torch.arange(
-                past_seen_tokens,
-                past_seen_tokens + inputs_embeds.shape[1],
-                device=inputs_embeds.device,
+            past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
+            cache_position: torch.Tensor = torch.arange(
+                past_seen_tokens, past_seen_tokens + inputs_embeds.shape[1], device=inputs_embeds.device
             )
 
-        inputs_embeds = inputs_embeds.type(torch.float32).cpu().numpy()
+        inputs_embeds_numpy: np.ndarray = inputs_embeds.type(torch.float32).cpu().numpy()
 
-        if inputs_embeds.ndim == 3:
-            inputs_embeds = np.expand_dims(
-                inputs_embeds, 1
-            )  # (batch, 1, seqlen, hidden_size)
+        if inputs_embeds_numpy.ndim == 3:
+            # (batch, 1, seqlen, hidden_size)
+            inputs_embeds_numpy = np.expand_dims(inputs_embeds_numpy, 1)
 
         # max width should be appropriate number for chunking (ex. 192 for Llama 3.2 3B)
         # it should be searched experimentally
         if chunk_size == 0:
             chunk_size = self.mxq_model.get_input_buffer_info()[0].max_width
-        num_of_chunks = math.ceil(inputs_embeds.shape[2] / chunk_size)
+        num_of_chunks = math.ceil(inputs_embeds_numpy.shape[2] / chunk_size)
 
         for i in range(num_of_chunks):
             start_index = i * chunk_size
-            end_index = min(start_index + chunk_size, inputs_embeds.shape[2])
-            cache_size = (
-                0 if past_key_values is None else past_key_values.get_seq_length()
-            )
+            end_index = min(start_index + chunk_size, inputs_embeds_numpy.shape[2])
+            cache_size = 0 if past_key_values is None else past_key_values.get_seq_length()
 
-            # last infer
-            if i == num_of_chunks - 1:
-                logits = self.mxq_model.infer(
-                    [inputs_embeds[:, :, start_index:end_index, :]], None, cache_size
-                )[0]
-            else:
-                logits = self.mxq_model.infer(
-                    [inputs_embeds[:, :, start_index:end_index, :]], None, cache_size
-                )[0]
+            outputs = self.mxq_model.infer([inputs_embeds_numpy[:, :, start_index:end_index, :]], None, cache_size)
 
-            if use_cache:
-                past_key_values.update_cache_position(
-                    cache_position[start_index:end_index]
-                )
+            if past_key_values is not None:
+                past_key_values.update_cache_position(cache_position[start_index:end_index])
 
-        logits = torch.tensor(logits, dtype=torch.float32, device=self.device).squeeze(0)
+        assert outputs is not None
+        logits: torch.FloatTensor = cast(torch.FloatTensor, torch.tensor(outputs[0], dtype=torch.float32).squeeze(0))
 
         loss = None
         if labels is not None:
-            loss = self.loss_function(
-                logits=logits,
-                labels=labels,
-                vocab_size=self.config.vocab_size,
-                **kwargs,
-            )
+            loss = self.loss_function(logits=logits, labels=labels, vocab_size=self.config.vocab_size, **kwargs)
 
         return CausalLMOutputWithPast(
             loss=loss,
@@ -181,9 +157,12 @@ class MobilintLlamaForCausalLM(LlamaPreTrainedModel, MobilintGenerationMixin):
             hidden_states=None,
             attentions=None,
         )
+    
+    def launch(self):
+        self.get_cache_mxq_model().launch(self.acc)
 
     def dispose(self):
-        self.mxq_model.dispose()
+        self.get_cache_mxq_model().dispose()
 
 
 AutoConfig.register("mobilint-llama", MobilintLlamaConfig)
--- mblt_model_zoo/transformers/large_language_model/llama_batch.py
+++ mblt_model_zoo/transformers/large_language_model/llama_batch.py
@@ -1,33 +1,35 @@
-from typing import Optional, Tuple, Union
+import math
+import os
+from typing import Optional, Tuple, Union, cast
 
 import maccel
+import numpy as np
 import torch
 import torch.nn as nn
-import numpy as np
-import math
-
 from transformers import (
-    LlamaPreTrainedModel,
-    LlamaConfig,
-    LlamaTokenizerFast,
     AutoConfig,
     AutoModel,
-    AutoTokenizer,
     AutoModelForCausalLM,
+    AutoTokenizer,
+    LlamaConfig,
+    LlamaPreTrainedModel,
+    LlamaTokenizerFast,
 )
-from transformers.processing_utils import Unpack
 from transformers.modeling_outputs import CausalLMOutputWithPast
-from transformers.utils import TransformersKwargs, logging
+from transformers.processing_utils import Unpack
+from transformers.utils import logging
+from transformers.utils.generic import TransformersKwargs
 
 from mblt_model_zoo.transformers.utils.generation_utils import MobilintGenerationMixin
-from ..utils.cache_utils import MobilintCache
+from mblt_model_zoo.utils.logging import log_model_details
 
+from ..utils.cache_utils import MobilintCache
 
 logger = logging.get_logger(__name__)
 
 
-class MobilintBatchLlamaConfig(LlamaConfig):
-    model_type = "mobilint-batch-llama"
+class MobilintLlamaBatchConfig(LlamaConfig):
+    model_type = "mobilint-llama-batch"
 
     def __init__(
         self,
@@ -45,9 +47,10 @@ class MobilintBatchLlamaConfig(LlamaConfig):
         self.tie_word_embeddings = False
 
 
-class MobilintBatchLlamaForCausalLM(LlamaPreTrainedModel, MobilintGenerationMixin):
-    config: MobilintBatchLlamaConfig
+class MobilintLlamaBatchForCausalLM(LlamaPreTrainedModel, MobilintGenerationMixin):
+    config: MobilintLlamaBatchConfig
     supports_gradient_checkpointing = False
+    _no_split_modules = []
     _supports_flash_attn = False
     _supports_sdpa = False
     _supports_flex_attn = False
@@ -56,31 +59,32 @@ class MobilintBatchLlamaForCausalLM(LlamaPreTrainedModel, MobilintGenerationMixi
     _supports_attention_backend = False
     _can_record_outputs = {}
 
-    def __init__(self, config: MobilintBatchLlamaConfig, *inputs, **kwargs):
+    def __init__(self, config: MobilintLlamaBatchConfig, *inputs, **kwargs):
         super().__init__(config, *inputs, **kwargs)
         self.padding_idx = config.pad_token_id
         self.vocab_size = config.vocab_size
 
-        self.embed_tokens = nn.Embedding(
-            config.vocab_size, config.hidden_size, self.padding_idx
-        )
+        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
         self.gradient_checkpointing = False
-
+        
         self.dev_no = config.dev_no
         self.acc = maccel.Accelerator(self.dev_no)
         mc = maccel.ModelConfig()
         mc.set_single_core_mode(config.num_cores)
-        self.mxq_model = maccel.Model(f"{config.name_or_path}/{config.mxq_path}", mc)
+        assert config.name_or_path is not None
+        model_path = os.path.join(config.name_or_path, config.mxq_path)
+        self.mxq_model = maccel.Model(model_path, mc)
+        log_model_details(model_path)
         self.mxq_model.launch(self.acc)
     
-    def get_mxq_model(self):
+    def get_cache_mxq_model(self):
         return self.mxq_model
 
     def set_decoder(self, decoder):
-        raise NotImplementedError("self.model is implemented in mxq")
+        raise NotImplementedError("set_decoder not available: self.model is implemented in mxq")
 
     def get_decoder(self):
-        logger.warning_once("self.model is implemented in mxq")
+        logger.warning("get_decoder not available: self.model is implemented in mxq")
         return None
 
     def tie_weights(self):
@@ -101,16 +105,16 @@ class MobilintBatchLlamaForCausalLM(LlamaPreTrainedModel, MobilintGenerationMixi
         **kwargs: Unpack[TransformersKwargs],
     ) -> Union[Tuple, CausalLMOutputWithPast]:
         if logits_to_keep > 1:
-            logger.warning("logits_to_keep larger than 1 is not supported: %d"% logits_to_keep)
+            logger.warning("logits_to_keep larger than 1 is not supported: %d" % logits_to_keep)
 
         if (input_ids is None) ^ (inputs_embeds is not None):
             raise ValueError("You must specify exactly one of input_ids or inputs_embeds")
 
         if inputs_embeds is None:
-            inputs_embeds: torch.Tensor = self.embed_tokens(input_ids)
-
+            inputs_embeds: torch.FloatTensor = self.embed_tokens(input_ids)
+        
         if use_cache and past_key_values is None:
-            past_key_values = MobilintCache(config=self.config)
+            past_key_values = MobilintCache(self.get_cache_mxq_model())
 
         if cache_position is None:
             past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
@@ -118,22 +122,21 @@ class MobilintBatchLlamaForCausalLM(LlamaPreTrainedModel, MobilintGenerationMixi
                 past_seen_tokens, past_seen_tokens + inputs_embeds.shape[1], device=inputs_embeds.device
             )
 
-        inputs_embeds = inputs_embeds.type(torch.float32).cpu().numpy()
+        inputs_embeds_numpy: np.ndarray = inputs_embeds.type(torch.float32).cpu().numpy()
 
-        if inputs_embeds.ndim == 3:
-            inputs_embeds = np.expand_dims(
-                inputs_embeds, 1
-            )  # (batch, 1, seqlen, hidden_size)
+        if inputs_embeds_numpy.ndim == 3:
+            # (batch, 1, seqlen, hidden_size)
+            inputs_embeds_numpy = np.expand_dims(inputs_embeds_numpy, 1)
 
         # max width should be appropriate number for chunking (ex. 192 for Llama 3.2 3B)
         # it should be searched experimentally
         if chunk_size == 0:
             chunk_size = self.mxq_model.get_input_buffer_info()[0].max_width
-        num_of_chunks = math.ceil(inputs_embeds.shape[2] / chunk_size)
+        num_of_chunks = math.ceil(inputs_embeds_numpy.shape[2] / chunk_size)
 
         for i in range(num_of_chunks):
             start_index = i * chunk_size
-            end_index = min(start_index + chunk_size, inputs_embeds.shape[2])
+            end_index = min(start_index + chunk_size, inputs_embeds_numpy.shape[2])
             cache_size = 0 if past_key_values is None else past_key_values.get_seq_length()
             batch_param = maccel.BatchParam(
                 sequence_lengths=[end_index - start_index],
@@ -142,21 +145,17 @@ class MobilintBatchLlamaForCausalLM(LlamaPreTrainedModel, MobilintGenerationMixi
                 prefill_masks=[False], # not implemented in C++ yet.
             )
 
-            logits = self.mxq_model.infer([inputs_embeds[:, :, start_index:end_index, :]], None, None, batch_param)[0]
+            outputs = self.mxq_model.infer([inputs_embeds_numpy[:, :, start_index:end_index, :]], None, None, batch_param)
 
-            if use_cache:
+            if past_key_values is not None:
                 past_key_values.update_cache_position(cache_position[start_index:end_index])
-
-        logits = torch.tensor(logits, dtype=torch.float32).squeeze(0)
+        
+        assert outputs is not None
+        logits: torch.FloatTensor = cast(torch.FloatTensor, torch.tensor(outputs[0], dtype=torch.float32).squeeze(0))
 
         loss = None
         if labels is not None:
-            loss = self.loss_function(
-                logits=logits,
-                labels=labels,
-                vocab_size=self.config.vocab_size,
-                **kwargs,
-            )
+            loss = self.loss_function(logits=logits, labels=labels, vocab_size=self.config.vocab_size, **kwargs)
 
         return CausalLMOutputWithPast(
             loss=loss,
@@ -166,14 +165,17 @@ class MobilintBatchLlamaForCausalLM(LlamaPreTrainedModel, MobilintGenerationMixi
             attentions=None,
         )
 
+    def launch(self):
+        self.get_cache_mxq_model().launch(self.acc)
+
     def dispose(self):
-        self.mxq_model.dispose()
+        self.get_cache_mxq_model().dispose()
 
 
-AutoConfig.register("mobilint-batch-llama", MobilintBatchLlamaConfig)
-AutoModel.register(MobilintBatchLlamaConfig, MobilintBatchLlamaForCausalLM)
-AutoTokenizer.register(MobilintBatchLlamaConfig, fast_tokenizer_class=LlamaTokenizerFast)
-AutoModelForCausalLM.register(MobilintBatchLlamaConfig, MobilintBatchLlamaForCausalLM)
+AutoConfig.register("mobilint-batch-llama", MobilintLlamaBatchConfig)
+AutoModel.register(MobilintLlamaBatchConfig, MobilintLlamaBatchForCausalLM)
+AutoTokenizer.register(MobilintLlamaBatchConfig, fast_tokenizer_class=LlamaTokenizerFast)
+AutoModelForCausalLM.register(MobilintLlamaBatchConfig, MobilintLlamaBatchForCausalLM)
 
 from ..utils.types import TransformersModelInfo
 
