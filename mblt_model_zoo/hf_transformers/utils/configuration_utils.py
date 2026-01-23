cf2620d7f9dfb8bac7158a85f4fd59a06e5ec271
beomsu <beomsu@mobilint.com>
2026-01-23 18:23:56 +0900

    Fix get_text_config

--- mblt_model_zoo/hf_transformers/utils/configuration_utils.py
+++ mblt_model_zoo/hf_transformers/utils/configuration_utils.py
@@ -1,4 +1,5 @@
 from typing import Any
+import copy
 
 from transformers.configuration_utils import (
     PretrainedConfig,
@@ -15,26 +16,6 @@ class MobilintConfigMixin(PretrainedConfig):
     ):
         self.npu_backend = MobilintNPUBackend.from_dict(kwargs, prefix="")
         super().__init__(**kwargs)
-    
-    @property
-    def mxq_path(self): return self.npu_backend.mxq_path
-    @mxq_path.setter
-    def mxq_path(self, v): self.npu_backend.mxq_path = v
-
-    @property
-    def dev_no(self): return self.npu_backend.dev_no
-    @dev_no.setter
-    def dev_no(self, v): self.npu_backend.dev_no = v
-
-    @property
-    def core_mode(self): return self.npu_backend.core_mode
-    @core_mode.setter
-    def core_mode(self, v): self.npu_backend.core_mode = v
-    
-    @property
-    def target_cores(self): return self.npu_backend.target_cores
-    @target_cores.setter
-    def target_cores(self, v): self.npu_backend.target_cores = v
     
     def _remove_keys_not_serialized(self, d: dict[str, Any]) -> None:
         if hasattr(self, "npu_backend"):
@@ -56,47 +37,7 @@ class MobilintEncoderDecoderConfigMixin(PretrainedConfig):
         self.encoder_npu_backend = MobilintNPUBackend.from_dict(kwargs, prefix="encoder_")
         self.decoder_npu_backend = MobilintNPUBackend.from_dict(kwargs, prefix="decoder_")
         super().__init__(**kwargs)
-    
-    @property
-    def encoder_mxq_path(self): return self.encoder_npu_backend.mxq_path
-    @encoder_mxq_path.setter
-    def encoder_mxq_path(self, v): self.encoder_npu_backend.mxq_path = v
-
-    @property
-    def encoder_dev_no(self): return self.encoder_npu_backend.dev_no
-    @encoder_dev_no.setter
-    def encoder_dev_no(self, v): self.encoder_npu_backend.dev_no = v
-
-    @property
-    def encoder_core_mode(self): return self.encoder_npu_backend.core_mode
-    @encoder_core_mode.setter
-    def encoder_core_mode(self, v): self.encoder_npu_backend.core_mode = v
-    
-    @property
-    def encoder_target_cores(self): return self.encoder_npu_backend.target_cores
-    @encoder_target_cores.setter
-    def encoder_target_cores(self, v): self.encoder_npu_backend.target_cores = v
-    
-    @property
-    def decoder_mxq_path(self): return self.decoder_npu_backend.mxq_path
-    @decoder_mxq_path.setter
-    def decoder_mxq_path(self, v): self.decoder_npu_backend.mxq_path = v
-
-    @property
-    def decoder_dev_no(self): return self.decoder_npu_backend.dev_no
-    @decoder_dev_no.setter
-    def decoder_dev_no(self, v): self.decoder_npu_backend.dev_no = v
-
-    @property
-    def decoder_core_mode(self): return self.decoder_npu_backend.core_mode
-    @decoder_core_mode.setter
-    def decoder_core_mode(self, v): self.decoder_npu_backend.core_mode = v
-    
-    @property
-    def decoder_target_cores(self): return self.decoder_npu_backend.target_cores
-    @decoder_target_cores.setter
-    def decoder_target_cores(self, v): self.decoder_npu_backend.target_cores = v
-    
+        
     def _remove_keys_not_serialized(self, d: dict[str, Any]) -> None:
         if hasattr(self, "encoder_npu_backend"):
             _ = d.pop("encoder_npu_backend", None)
@@ -108,6 +49,75 @@ class MobilintEncoderDecoderConfigMixin(PretrainedConfig):
 
     def to_dict(self):
         output = super().to_dict()
+
         output.update(self.encoder_npu_backend.to_dict(prefix="encoder_"))
         output.update(self.decoder_npu_backend.to_dict(prefix="decoder_"))
+
         return output
+
+    def get_text_config(self, decoder=None, encoder=None) -> "PretrainedConfig":
+        return_both = decoder == encoder  # both unset or both set -> search all possible names
+
+        decoder_possible_text_config_names = ("decoder", "generator", "text_config")
+        encoder_possible_text_config_names = ("text_encoder",)
+        if return_both:
+            possible_text_config_names = encoder_possible_text_config_names + decoder_possible_text_config_names
+        elif decoder:
+            possible_text_config_names = decoder_possible_text_config_names
+        else:
+            possible_text_config_names = encoder_possible_text_config_names
+
+        valid_text_config_names = []
+        for text_config_name in possible_text_config_names:
+            if hasattr(self, text_config_name):
+                text_config = getattr(self, text_config_name, None)
+                if text_config is not None:
+                    valid_text_config_names += [text_config_name]
+
+        if len(valid_text_config_names) > 1:
+            raise ValueError(
+                f"Multiple valid text configs were found in the model config: {valid_text_config_names}. In this "
+                "case, using `get_text_config()` would be ambiguous. Please specify the desired text config directly, "
+                "e.g. `text_config = config.sub_config_name`"
+            )
+        elif len(valid_text_config_names) == 1:
+            config_to_return = getattr(self, valid_text_config_names[0])
+        else:
+            config_to_return = self
+
+        # handle legacy models with flat config structure, when we only want one of the configs
+        if not return_both and len(valid_text_config_names) == 0 and config_to_return.is_encoder_decoder:
+            config_to_return = copy.deepcopy(config_to_return)
+            prefix_to_discard = "encoder" if decoder else "decoder"
+            prefix_to_keep = "decoder" if decoder else "encoder"
+            for key in config_to_return.to_dict():
+                # NOTE: We don't want to discard the key if it is mapped from a different attribute name at read time
+                if key.startswith(prefix_to_discard) and key not in config_to_return.attribute_map.values():
+                    try:
+                        delattr(config_to_return, key)
+                    except:
+                        pass
+                if key.startswith(prefix_to_keep):
+                    # [encoder/decoder]_layers -> num_hidden_layers
+                    if key == prefix_to_keep + "_layers":
+                        new_key = "num_hidden_layers"
+                    # [encoder/decoder]_attention_heads -> num_attention_heads
+                    elif key == prefix_to_keep + "_attention_heads":
+                        new_key = "num_attention_heads"
+                    # e.g. encoder_hidden_act -> hidden_act
+                    else:
+                        new_key = key[len(prefix_to_keep) + 1 :]
+
+                    # Does the class map the new key into a different attribute name at read time? if so, let's write
+                    # into that attribute instead
+                    if new_key in config_to_return.attribute_map:
+                        new_key = config_to_return.attribute_map[new_key]
+                    
+                    try:
+                        value = getattr(config_to_return, key)
+                        delattr(config_to_return, key)
+                        setattr(config_to_return, new_key, value)
+                    except:
+                        pass
+
+        return config_to_return
