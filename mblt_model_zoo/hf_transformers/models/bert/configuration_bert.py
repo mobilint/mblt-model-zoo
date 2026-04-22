59e760f171a64ce150a74f164d1b0464dcf74f59
beomsu <beomsu@mobilint.com>
2026-04-22 15:07:15 +0900

    Fix bert buffer initialization

--- mblt_model_zoo/hf_transformers/models/bert/configuration_bert.py
+++ mblt_model_zoo/hf_transformers/models/bert/configuration_bert.py
@@ -1,14 +1,23 @@
+from typing import Any
+
 from transformers.models.auto.configuration_auto import AutoConfig
 from transformers.models.auto.tokenization_auto import AutoTokenizer
 from transformers.models.bert.configuration_bert import BertConfig
 from transformers.models.bert.tokenization_bert import BertTokenizer
-from transformers.models.bert.tokenization_bert_fast import BertTokenizerFast
+
+try:
+    from transformers.models.bert.tokenization_bert_fast import BertTokenizerFast
+except ImportError:
+    from transformers.models.bert.tokenization_bert import BertTokenizerFast
 
 from ...utils.configuration_utils import MobilintConfigMixin
 
 
 class MobilintBertConfig(MobilintConfigMixin, BertConfig):
     model_type = "mobilint-bert"
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
     
 AutoConfig.register("mobilint-bert", MobilintBertConfig)
 AutoTokenizer.register(MobilintBertConfig, slow_tokenizer_class=BertTokenizer, fast_tokenizer_class=BertTokenizerFast)
