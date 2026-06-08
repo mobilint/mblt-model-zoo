fix: robustly handle YOLO outputs and ONNX input layout normalization

mblt_model_zoo/vision/utils/postprocess/base.py
@@ -259,14 +259,24 @@ class YOLOPostBase(PostBase):
         if isinstance(x, list):
             if not x:
                 return None, None
-            first = x[0]
-            normalized_first = self._normalize_final_detection_tensor(first, final_det_dim)
-            if normalized_first is not None:
-                detections = self._final_detection_batches(normalized_first)
-                proto = None
-                if len(x) > 1 and isinstance(x[1], (np.ndarray, torch.Tensor)):
-                    proto = self._normalize_proto_batch(x[1])
-                return detections, proto
+
+            normalized_detections: np.ndarray | torch.Tensor | None = None
+            normalized_proto: torch.Tensor | None = None
+            for output in x:
+                if not isinstance(output, (np.ndarray, torch.Tensor)):
+                    continue
+                if normalized_detections is None:
+                    normalized_detections = self._normalize_final_detection_tensor(output, final_det_dim)
+                    if normalized_detections is not None:
+                        continue
+                if normalized_proto is None:
+                    try:
+                        normalized_proto = self._normalize_proto_batch(output)
+                    except ValueError:
+                        continue
+
+            if normalized_detections is not None:
+                return self._final_detection_batches(normalized_detections), normalized_proto
             return None, None
 
         normalized_x = self._normalize_final_detection_tensor(x, final_det_dim)

mblt_model_zoo/vision/wrapper.py
@@ -372,13 +372,65 @@ class MBLT_Engine:
         expected_shape = self._require_onnx_session().get_inputs()[0].shape
         if len(expected_shape) == 4:
             if x_np.ndim == 3:
-                if x_np.shape[0] == 3:
+                first_channel_count = x_np.shape[0]
+                last_channel_count = x_np.shape[-1]
+            elif x_np.ndim == 4:
+                first_channel_count = x_np.shape[1]
+                last_channel_count = x_np.shape[-1]
+            else:
+                first_channel_count = None
+                last_channel_count = None
+
+            expected_second_dim = expected_shape[1]
+            expected_last_dim = expected_shape[-1]
+            second_matches_channels = isinstance(expected_second_dim, int) and (
+                expected_second_dim == first_channel_count or expected_second_dim == last_channel_count
+            )
+            last_matches_channels = isinstance(expected_last_dim, int) and (
+                expected_last_dim == first_channel_count or expected_last_dim == last_channel_count
+            )
+
+            expected_layout = None
+            expected_channels = None
+            if second_matches_channels and not last_matches_channels:
+                expected_layout = "nchw"
+                expected_channels = expected_second_dim
+            elif last_matches_channels and not second_matches_channels:
+                expected_layout = "nhwc"
+                expected_channels = expected_last_dim
+            elif isinstance(expected_second_dim, int) and expected_second_dim in {1, 2, 3, 4}:
+                expected_layout = "nchw"
+                expected_channels = expected_second_dim
+            elif isinstance(expected_last_dim, int) and expected_last_dim in {1, 2, 3, 4}:
+                expected_layout = "nhwc"
+                expected_channels = expected_last_dim
+
+            if x_np.ndim == 3:
+                if expected_layout == "nchw" and expected_channels is not None and x_np.shape[0] == expected_channels:
                     x_np = np.expand_dims(x_np, axis=0)
-                elif x_np.shape[-1] == 3:
+                elif (
+                    expected_layout == "nchw" and expected_channels is not None and x_np.shape[-1] == expected_channels
+                ):
                     x_np = np.transpose(x_np, (2, 0, 1))
                     x_np = np.expand_dims(x_np, axis=0)
-            elif x_np.ndim == 4 and x_np.shape[-1] == 3 and x_np.shape[1] != 3:
-                x_np = np.transpose(x_np, (0, 3, 1, 2))
+                elif (
+                    expected_layout == "nhwc" and expected_channels is not None and x_np.shape[-1] == expected_channels
+                ):
+                    x_np = np.expand_dims(x_np, axis=0)
+                elif expected_layout == "nhwc" and expected_channels is not None and x_np.shape[0] == expected_channels:
+                    x_np = np.transpose(x_np, (1, 2, 0))
+                    x_np = np.expand_dims(x_np, axis=0)
+            elif x_np.ndim == 4:
+                if expected_layout == "nchw" and expected_channels is not None:
+                    if x_np.shape[1] == expected_channels:
+                        pass
+                    elif x_np.shape[-1] == expected_channels:
+                        x_np = np.transpose(x_np, (0, 3, 1, 2))
+                elif expected_layout == "nhwc" and expected_channels is not None:
+                    if x_np.shape[-1] == expected_channels:
+                        pass
+                    elif x_np.shape[1] == expected_channels:
+                        x_np = np.transpose(x_np, (0, 2, 3, 1))
 
         return {self.input_name: x_np}
 